537e02aa6c768f1ea7c90c429f5666e31108b53f
prof-salu <saluorodrigues@gmail.com>
2020-08-30 16:48:17 -0300

    Padrões Criacionais

--- DesignPatterns-creational/src/builder/ConversorASCII.java
+++
@@ -1,7 +0,0 @@
-package builder;
-
-public class ConversorASCII extends ConversorTexto {
-	void converterCaractere(char c) {
-		System.out.println("CaractereASCII");
-	}
-}
--- DesignPatterns-creational/src/builder/ConversorTexto.java
+++
@@ -1,9 +0,0 @@
-package builder;
-
-public abstract class ConversorTexto {
-	void converterCaractere(char c) {}
-
-	void converterParagrafo() {}
-
-	void converterFonte(Fonte f) {}
-}
